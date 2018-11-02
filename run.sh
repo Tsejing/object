5821a10cfe09634c29978a85b0f46bf22d1ade47
yuanpeng <43242776+yuanpeng50@users.noreply.github.com>
2018-11-03 04:38:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
 output_dir=/output  # 训练目录
-dataset_dir=/data/yuanpeng/yp # 数据集目录，这里是写死的，记得修改
+dataset_dir=/data/yuanpeng/vdami-f # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
@@ -20,6 +20,7 @@ eval_dir=$output_dir/eval
 config=ssd_mobilenet_v1_pets.config
 pipeline_config_path=$output_dir/$config
 
+cp ./$config $pipeline_config_path
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
 # tinymind已经支持引用上一次的运行结果，这一行需要删掉，不然会出现上一次的运行结果被清空的状况。
 # rm -rvf $output_dir/*
